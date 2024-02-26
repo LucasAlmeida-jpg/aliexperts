2eed9adcf33858ad11be0137b7b52e3db97d93c5
Bruno Xavier <bruno.xavier@creators.llc>
2024-02-26 15:52:51 -0300

    teste

--- js/all.js
+++ js/all.js
@@ -227,6 +227,7 @@ createApp({
       })
       .then(data => {
         if (data) {
+          console.log(data.data.date_subscription, 'teste' );
           if(data.data.date_subscription != null){
             const now = new Date();
             const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`;
