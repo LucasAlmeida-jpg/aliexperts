0db75afb844adf98c2592e1047ce4a6a1d8ceb21
Bruno Xavier <bruno.xavier@creators.llc>
2024-02-26 16:09:35 -0300

    ajustes

--- js/all.js
+++ js/all.js
@@ -228,7 +228,7 @@ createApp({
       .then(data => {
         if (data) {
           console.log(data.data.user.date_subscription)
-          if(data.data.user && data.data.user.date_subscription != null){
+          if(data.data.user && data.data.user.date_subscription == null){
             const now = new Date();
             const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`;
             this.formData.date_subscription = formattedDate;
